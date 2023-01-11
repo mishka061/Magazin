f05571f69d4a4e71d69749993b3c092c2d14f3b4
Алена Котова <mishka061@rambler.ru>
2023-01-11 20:16:46 +0300

    добавление urls cart в проект

---
+++ cart/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('', views.cart_detail, name='cart_detail'),# Адрес корзины полностью
+    path('add/<int:product_id>/',
+         views.cart_add,
+         name='cart_add'),# Добавление корзины
+    path('remove/<int:product_id>/',
+         views.cart_remove,
+         name='cart_remove'),# Удаление корзины
+]
