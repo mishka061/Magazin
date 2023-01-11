2a3b54f6b82b0c04cf27953048ea4063ea4f4769
Алена Котова <mishka061@rambler.ru>
2023-01-11 11:03:36 +0300

    добавление и миграция моделей

---
+++ myshop/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_urls(self):
+        return reverse('myshop:product_list_by_category', args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+
+    name = models.CharField(
+        max_length=150,
+        db_index=True)
+    slug = models.CharField(
+        max_length=150,
+        db_index=True,
+        unique=True)
+    image = models.ImageField(
+        upload_to='product/%Y/%m/%d',
+        blank=True)
+    description = models.TextField(max_length=1000, blank=True)  # Описание
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Цена
+    available = models.BooleanField(default=True)  # Наличие
+    created = models.DateTimeField(auto_now_add=True)  # Добавление
+    uploaded = models.DateTimeField(auto_now=True)  # Обновление
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_urls(self):
+        return reverse('myshop:product_detail', args=[self.id, self.slug])
